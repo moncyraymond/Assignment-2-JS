c7e9fb54b7e20c6d4eaacdd301f7dcaa6e0e4e8d
moncyraymond <126798719+moncyraymond@users.noreply.github.com>
2023-03-13 07:14:58 +0400

    Add files via upload
    HTML AND JS FILE UPLOADED FOR ASSIGNMENT 2

---
+++ JavaScript-Assignment-A.js
@@ -0,0 +1,94 @@
+/*
+     JavaScript Assignment A
+     ASSIGNMENT NOTES:
+     Do not change the code below. 
+     Add your code in the 'Answers Section'.
+*/
+
+var cities = [
+    'Abu Dhabi',
+    'Abuja',
+    'Cairo',
+    '???',
+    'Amman',
+    'Islamabad',
+    'Tokyo',
+    'Beirut',
+    '???',
+    'Ottowa',
+    'New Delhi',
+    'Brasília'
+];
+
+var countries = [
+    'UAE',
+    'Nigeria',
+    'Egypt',
+    '???',
+    'Jordan',
+    'Pakistan',
+    'Japan',
+    'Lebanon',
+    '???',
+    'Canada',
+    'India',
+    'Brazil'
+];
+
+
+
+/******** Answers Section ********/
+
+// 1. Create a for loop to iterate over 'cities'
+
+for (var i=cities; i<=cities.length; i++ )
+console.log (cities);
+
+console.log (cities);
+
+// 2. Inside the for loop, console.log() the country for that city (e.g, "Abu Dhabi, UAE")
+// Hint: use a counter variable to access both 'cities' and 'countries'
+
+var counter = [
+    ['Abu Dhabi','UAE'],
+    ['Abuja','Nigeria'],
+    ['Cairo','Egypt'],
+    ['???','???'],
+    ['Amman','Jordan'],
+    ['Islamabad','Pakistan'],
+    ['Tokyo','Japan'],
+    ['Beirut','Lebanon'],
+    ['???','???']
+    ['Ottowa','Canada'],
+    ['New Delhi', 'India'],
+    ['Brasilia', 'Brazil']  
+]
+
+for (var i=counter; i<=12; i++); {
+
+console.log (counter);
+
+console.log(counter);
+
+
+
+// 3. If the name of a city is missing (i.e there is '???'), use console.log("Oops!")
+
+if(cities='???') {
+    console.log ("Oops!");}
+
+    else {
+    console.log (counter);}
+
+    console.log (counter)
+
+// NOTE: For 2 and 3, use if/else. There should be no '???' in the console!
+    
+}
+
+if(counter='???', '???') {
+    counter.push
+}
+
+console.log (counter)
+
